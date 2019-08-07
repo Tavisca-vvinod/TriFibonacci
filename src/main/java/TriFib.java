30cd398a85a640b5e7b6aa7d3739636f4e71dc26
vvinod <vvinod@tavisca.in>
2019-08-07 15:07:27 +0530

    Completed case in which position of -1 is ahead 2 in the question array

--- src/main/java/TriFib.java
+++ src/main/java/TriFib.java
@@ -1,14 +1,51 @@
 public class TriFib {
     public static void main(String[] args)
     {
-        System.out.println("The missing number is "+validateFibonacci(new int[] {1,2,3,6,11}));
+        System.out.println("The missing number is "+complete(new int[] {1,2,3,-1,11,20}));
     }
-
     private static int complete(int[] numbers) {
 
-        return -1;
+        int indexOfMissingNumber= indexOf(numbers);
+        if(indexOfMissingNumber>=3)
+        {
+            int[] generatedFibonacci= generateFibonacci(numbers);
+            for(int i=0;i<generatedFibonacci.length;i++)
+            {
+                if(generatedFibonacci[i]!=numbers[i] && i!=indexOfMissingNumber) {
+                    return -1;
+                }
+            }
+            return generatedFibonacci[indexOfMissingNumber];
+        }
+        return 0;
     }
-
+    private static int[] generateFibonacci(int[] numbers)
+    {
+        int[] generatedFibonacci=new int[numbers.length];
+        for(int i=0;i<numbers.length;i++)
+        {
+            if(i<3)
+            {
+                generatedFibonacci[i]=numbers[i];
+            }
+            else
+            {
+                generatedFibonacci[i]=generatedFibonacci[i-1]+generatedFibonacci[i-2]+generatedFibonacci[i-3];
+            }
+        }
+        return generatedFibonacci;
+    }
+    private static int indexOf(int[] numbers)
+    {
+        for(int i=0;i<numbers.length;i++)
+        {
+            if(numbers[i]==-1)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
     private static boolean validateFibonacci(int [] numbers)
     {
         boolean valid=true;
