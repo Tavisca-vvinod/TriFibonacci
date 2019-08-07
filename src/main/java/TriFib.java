8fb57be80633828613bdbeaf905af4b933766f97
vvinod <vvinod@tavisca.in>
2019-08-07 14:05:11 +0530

    Added a function to validate a fibonacci sequence

--- src/main/java/TriFib.java
+++ src/main/java/TriFib.java
@@ -1,10 +1,25 @@
 public class TriFib {
     public static void main(String[] args)
     {
-        System.out.println("The missing number is "+complete(new int[] {1,2,3,-1}));
+        System.out.println("The missing number is "+validateFibonacci(new int[] {1,2,3,6,11}));
     }
 
     private static int complete(int[] numbers) {
+
         return -1;
+    }
+
+    private static boolean validateFibonacci(int [] numbers)
+    {
+        boolean valid=true;
+        for(int i=3;i<numbers.length;i++)
+        {
+            if(numbers[i]!=numbers[i-1]+numbers[i-2]+numbers[i-3])
+            {
+                valid=false;
+                break;
+            }
+        }
+        return valid;
     }
 }
