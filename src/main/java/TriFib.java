9e7752875b31892ff7bdc98c53ebbb269cf4eb08
vvinod <vvinod@tavisca.in>
2019-08-07 13:57:35 +0530

    Initial commit

---
+++ src/main/java/TriFib.java
@@ -0,0 +1,10 @@
+public class TriFib {
+    public static void main(String[] args)
+    {
+        System.out.println("The missing number is "+complete(new int[] {1,2,3,-1}));
+    }
+
+    private static int complete(int[] numbers) {
+        return -1;
+    }
+}
